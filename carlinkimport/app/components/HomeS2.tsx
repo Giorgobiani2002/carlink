e23b3443478794d32548a57eac77c4aaa10fcc71
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-11-21 14:28:30 +0400

    newF/homesection3

--- carlinkimport/app/components/HomeS2.tsx
+++ carlinkimport/app/components/HomeS2.tsx
@@ -12,13 +12,10 @@ import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
 
 export default function HomeS2() {
   return (
-    <div className="bg-black h-[630px] flex py-10 font-sans">
-      <div className="max-w-[1220px] w-full m-auto flex justify-between font-sans gap-2.5">
-        <div className="flex h-[450px] max-w-[550px] justify-between flex-col ">
+    <div className="h-[630px] bg-[#1F1F1F] flex py-10 font-sans">
+      <div className="max-w-[1220px] w-full m-auto items-center flex justify-between font-sans gap-2.5">
+        <div className="flex h-[400px] max-w-[550px] justify-between flex-col ">
           <div>
-            <h4 className="text-green-900 font-extrabold text-[18px]">
-              CARLINK
-            </h4>
             <h2 className="text-[35px] text-white leading-9">
               <span className="text-green-900 leading-9">
                 შეიძინე ავტომობილი
@@ -26,7 +23,7 @@ export default function HomeS2() {
               საუკეთესო პირობებით
             </h2>
           </div>
-          <p className="text-gray-600 text-[18px]">
+          <p className="text-white text-[18px]">
             სანდო პარტნიორები და პროფესიონალი გუნდი – სწორედ ეს ქმნის იმ
             სტაბილურობას, რომელსაც ჩვენი კლიენტები აფასებენ. ჩვენთან ყოველი
             ნაბიჯი დაგეგმილია : დაწყებული ავტომობილის შერჩევით, დასრულებული მის
@@ -58,11 +55,14 @@ export default function HomeS2() {
               disableOnInteraction: false,
             }}
             modules={[EffectCoverflow, Pagination, Autoplay]}
-            className="max-w-[600px] w-full h-[450px]"
+            className="max-w-[650px] w-full h-[450px]"
           >
-            {["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.jpg"].map(
+            {["slide11.png", "slide22.png", "slide3.jpg", "slide4.jpg"].map(
               (src, idx) => (
-                <SwiperSlide key={idx} className="w-[600px] h-[380px] rounded-2xl">
+                <SwiperSlide
+                  key={idx}
+                  className="w-[600px] h-[380px] rounded-2xl"
+                >
                   <img
                     src={src}
                     className="w-full h-full object-cover rounded-2xl"
